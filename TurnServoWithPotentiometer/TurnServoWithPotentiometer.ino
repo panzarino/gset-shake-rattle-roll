e84904194cd85b0ef6ec78baabff871af897bd8e
zachpanz88 <zachary@panzarino.com>
2016-06-30 18:11:50 -0400

    Starting with servos

---
+++ TurnServoWithPotentiometer/TurnServoWithPotentiometer.ino
@@ -0,0 +1,14 @@
+#include <Servo.h>
+
+Servo servo;
+
+void setup() {
+  servo.attach(9);
+}
+
+void loop() {
+  int val = analogRead(A0);
+  val = map(val, 0, 1023, 0, 180);
+  servo.write(val);
+  delay(15);
+}
