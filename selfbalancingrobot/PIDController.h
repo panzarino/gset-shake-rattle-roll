3eb756d0ed13f3c875a348ac4b939af87106ce79
reimuzerg <17huangd@millburn.org>
2016-07-11 17:53:21 -0400

    finished PID controller; ready for tuning

--- selfbalancingrobot/PIDController.h
+++ selfbalancingrobot/PIDController.h
@@ -1,60 +1,80 @@
 #include "Arduino.h"
 
+/**
+ * creeds to Emily Liu for teaching me to write PID derps
+ */
+
 class PIDController {
   
   public:
   
-  float target;          // setpoint
-  float kp, ki, kd;      // gain constants
-  unsigned long prevtime;
-  float lastInput, sigma, output, max_, min_;
+  float sp;                                      // setpoint
+  float kp, ki, kd;                              // gain constants
+  unsigned long prevtime;                        // previous time stamp
+  float prevval, integral, output, max_, min_;   // previous input, integral, output, max/min output values
   
-  // empty initializer
-  PIDController() {};
-  
-  // initializer with no function
-  PIDController(float minOut, float maxOut, float setpoint = 0,
-                float kP = 0.0, float kI = 0.0, float kD = 0.0) {
-    max_ = maxOut;
-    min_ = minOut;
-    setTarget(setpoint, kP, kI, kD);
+  /** 
+   * initializer PIDController
+   * min and max are min and max motor output respectively
+   * setpoint is the target sensor combined value we want
+   * kP, kI, and kD are the gain constants we want and we will need to tune them
+   */
+  PIDController(float min, float max, float setpoint, float kP, float kI, float kD) {
+    max_ = max;         // set max output 
+    min_ = min;         // set min output
+    setsp(setpoint);            // set set point
+    setconstants(kP, kI, kD);   // sets gain constants
   }
   
-  // update values and calculate
-  float calculate(float current) {
-    
-    // time check
-    unsigned long now = millis();
-    double dt = (double) (now - lastTime);
-    
-    // p, i, d updates
-    double error = target - current;
-    sigma += (error * dt);
-    double slope = (lastInput - current) / dt;
-    
-    // total and bound pid output
-    output = kp * error + ki * sigma + kd * slope;
-    output = output > max_ ? max_ : output;
-    output = output < min_ ? min_ : output;
-    
+  /** 
+   * update values and calculate
+   * curval is the curval sensor reading
+   */
+  float calculate(float curval) {
+    // get curval time
+    unsigned long curtime = millis();
+    double dt = (double) (curtime - prevtime);      // dt
+    // calculate error from set point
+    double error = sp - curval;
+    integral += (error * dt);                // update intgral by Riemann sum
+    double de = (prevval - curval) / dt;  // de, difference in error over time
+    /* P only control*/
+    // output  = kp * error
+    /* PI control */
+    // output = kp * error + ki * integral
+    /* PD control */
+    // output = kp * error + kd * de
+    /* PID control */
+    output = kp * error + ki * integral + kd * de;
+    // check if within bounds
+    if (output > max_) {
+      return max_;
+    }
+    else if (output < min_) {
+      return min_;
+    }
+    else {
+      return output;
+    }
     // update variables and return
-    lastTime = now;
-    lastInput = current;
+    prevtime = curtime;
+    prevval = curval;
     return output;
   }
   
-  // set new target with new pid constants, if given; otherwise keep same
-  void setTarget(float setpoint, float kP = -999, float kI = -999, float kD = -999) {
-    target = setpoint;
-    if (kP != -999) kp = kP;
-    if (kI != -999) ki = kI;
-    if (kD != -999) kd = kD;
-    lastTime = millis();
-    lastInput = 0;
+  /**
+   * set new sp with new pid constant
+   */
+  void setsp(float setpoint) {
+    sp = setpoint;
+    prevtime = millis();
+    prevval = 0;
   }
   
-  // set new constants
-  void setConstants(float kP, float kI, float kD) {
+  /** 
+   * set new constants
+   */
+  void setconstants(float kP, float kI, float kD) {
     kp = kP;
     ki = kI;
     kd = kD;
